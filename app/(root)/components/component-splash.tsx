7f5d5397c5768957fddf6073e589424512a73095
Adrian <adrianhenry2115@gmail.com>
2025-02-07 13:14:21 -0500

    component splash error

--- app/(root)/components/component-splash.tsx
+++ app/(root)/components/component-splash.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-import Logo from "../public/assets/icons/brite-logo.png";
+import Logo from "../../../public/assets/icons/brite-logo.png";
 
 interface ISplashProps {
     title: string;
